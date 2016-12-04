c853155e51c2a3449c5ecb2ceb019c7cf10d88db
nsperkin <nsperkins13@gmail.com>
2016-12-03 22:32:29 -0800

    done

---
+++ script.py
@@ -0,0 +1,27 @@
+import os
+import sys
+import argparse
+
+def pipeline():
+    # check args
+
+    if len(sys.argv) < 4:
+        print("USAGE: python script.py <ref> <infile>")
+        return
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('file', nargs=3)
+    parser.add_argument('--bwa_path', default='bwa', help='Destination of bwa')
+    parser.add_argument('--samtools_path', default='samtools', help='Destination of samtools')
+    parser.add_argument('--freebayes_path', default='freebayes', help='Destination of freebayes')
+    parser.add_argument('--snpEff_path', default='snpEff.jar', help='Destination of snpEff')
+
+    args = parser.parse_args()
+
+    os.system(args.bwa_path + " index " + args.file[0])
+    os.system(args.bwa_path + " mem -M -t 16 " + args.file[0] + " " + args.file[1] + " > output.sam")
+    os.system(args.samtools_path + ' view -Sb output.sam | ' + args.samtools_path + ' sort -o output.bam')
+    os.system(args.freebayes_path + " -f " + args.file[0] + " output.bam > output.vcf")
+    os.system("java -jar " + args.snpEff_path + " " + args.file[2] + " output.vcf > output.ann.vcf")
+
+pipeline()
